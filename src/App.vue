79e00e01571990d663ea059191fdf7568494a0b8
2UN7 <1491859758@qq.com>
2021-08-14 18:30:37 +0800

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>姓名: {{ name }}</h1>
+  <h1>年龄: {{ age }}</h1>
+  <h1>地址: {{ address.provinse }}-{{ address.city }}</h1>
+  <button @click="changedUser">修改</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref } from "vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
+  setup() {
+    let name = ref("张三");
+    let age = ref("18");
+    let address = ref({
+      provinse: "山东",
+      city: "济南",
+    });
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    function changedUser() {
+      name.value = "李四";
+      age.value = 20;
+      address.value.provinse = "河北";
+      address.value.city = "石家庄";
+    }
+
+    return { name, age, address, changedUser };
+  },
+};
+</script>
